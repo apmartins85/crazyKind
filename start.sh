376b4728f50bfa8a15953b9b278ee1d69e7a48b3
apmartins <apmartins@gmx.com>
2021-08-10 19:11:25 -0300

    underconstruction localstack + crossplane

--- start.sh
+++ start.sh
@@ -11,6 +11,28 @@ kubectl cluster-info --context kind-kind
 sleep 5
 kubectl get nodes
 sleep 5
+echo "It is time to deploy localstack"
+echo "We will install using helm3"
+echo "We suppose that you already have installed helm3, Am I right? If so, it is show time!"
+sleep 5
+helm repo add localstack-repo https://helm.localstack.cloud
+helm repo update
+helm upgrade --install localstack localstack-repo/localstack
+sleep 5
+echo "It is time to deploy crossplane"
+echo "We will install using helm3"
+echo "We suppose that you already have installed helm3, Am I right? If so, it is show time!"
+kubectl create namespace crossplane-system
+helm repo add crossplane-master https://charts.crossplane.io/master/
+helm repo update
+# Kubernetes 1.15 and newer versions
+helm install crossplane --namespace crossplane-system crossplane-master/crossplane --version 1.2.3
+sleep 10
+kubectl get pods -n crossplane-system
+sleep 10
+kubectl get pods -n localstack
+sleep 10
 echo "Well done, for now, We will destroy everything!! 👿"
 kind delete cluster --name kind
+sleep 10
 osascript -e 'quit app "Docker"'
